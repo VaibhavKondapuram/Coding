5e8727b60fe5a5a1acf47338b8d1a3f5bdb7e463
Vaibhav kondapuram <vaibhavpawanism@gmail.com>
2023-07-31 18:53:26 +0530

    optimize the code

---
+++ myapp/src/Components/DetailCard/DCImg.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Styles.css";
+
+const DCImg = ({ Img }) => {
+  return (
+    <>
+      <img src={Img} alt="" className="dcImg" />
+    </>
+  );
+};
+
+export default DCImg;
