f142d6bba622fd3fe4bd2a05ade68674a629a9c5
Vaibhav kondapuram <vaibhavpawanism@gmail.com>
2023-08-01 21:04:37 +0530

    done

---
+++ Examples/Example2/objects.js
@@ -0,0 +1,14 @@
+const person = {
+    name : 'vaibhav',
+    company: 'accenture',
+    favNum: 10,
+    hasBlackHair: true,
+    favSnacks: ['Pasta', 'Veg Pizza'],
+    greeting: function(){
+        console.log('hello everybody!');
+    }
+};
+
+
+person.name;
+person.greeting();
