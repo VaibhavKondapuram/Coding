8fb0ae010c0513903d38e3379f1a0b9a7bc4761c
Vaibhav kondapuram <vaibhavpawanism@gmail.com>
2023-08-05 19:05:45 +0530

    done

---
+++ Examples/Example2/TheHardparts/functions.js
@@ -0,0 +1,18 @@
+var employee;
+function employee(){
+    name: "Vaibhav Kondapuram";
+    id : 312345;
+    domain: "Full-stack developer";
+    mailId: "vaibhavkondapuram2@gmail.com";
+}
+
+console.log(employee);
+
+
+var substract;
+function substract(num1, num2){
+    return num1 - num2;
+}
+
+const output = substract(150 - 132)
+console.log(substract);
