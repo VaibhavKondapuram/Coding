c4872797edd80291bdc51856cf465b68277465c5
Vaibhav kondapuram <vaibhavpawanism@gmail.com>
2023-08-26 12:41:07 +0530

    done

---
+++ Examples/Example2/DSAlgo/functions7.js
@@ -0,0 +1,6 @@
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+console.log(setUserName);
